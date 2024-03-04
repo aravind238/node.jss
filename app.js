ebafc380f8d9b5d0c16beddf15b693cd2b6c8990
aravind <aravindhrma@gmail.com>
2024-03-04 18:09:50 +0000

    Sec commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('./logger');
 
 app.get('/', (req, res) => {
   // Simulate fetching data (replace with your actual logic)
-  const data = { message: 'Hello from the API!' };
+  const data = { message: 'Hello from the Aravind!' };
   res.json(data); // Send structured JSON response
 });
 
