358850bdc11a31bf69927bd75f0fb22346aba64b
root <root@ip-172-31-93-55.ec2.internal>
2024-03-27 20:01:07 +0000

    15commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('./logger');
 
 app.get('/', (req, res) => {
   // Simulate fetching data (replace with your actual logic)
-  const data = { message: 'hi there3!' };
+  const data = { message: 'hi IOdata!' };
   res.json(data); // Send structured JSON response
 });
 
